77e1b1cacc0dfe6d6ef84122a0e13398818eba77
Даниил <sergeevdaniil383@gmail.com>
2025-09-25 20:40:36 +0300

    3d

--- src/Main.java
+++ src/Main.java
@@ -14,7 +14,7 @@ public class Main {
         int personY = 3;
 
         int step = 0;
-        // \n, \t - спец символ
+        // \n, \t - как спец символ(упомянуть)
         String gamingField = "+ —— + —— + —— +\n"
                 + "|    |    | \uD83C\uDFE0 |\n"
                 + "+ —— + —— + —— +\n"
@@ -29,9 +29,75 @@ public class Main {
         String answer = sc.nextLine();
         System.out.println("Ваш ответ:\t" + answer);
 
-        System.out.println("Выбери сложность игры(от 1 до 5):");
-        int difficultGame = sc.nextInt();
-        System.out.println("Выбранная сложность:\t" + difficultGame);
+
+
+//        if (answer.equals("ДА")) {
+
+//        System.out.println("Выбери сложность игры(от 1 до 5):");
+//        int difficultGame = sc.nextInt();
+//        System.out.println("Выбранная сложность:\t" + difficultGame);
+//            System.out.println("Сколько жизней будет у персонажа?");
+////            personLive = sc.nextInt(); // здесь необходимо рассказать о возможных ошибках(неверный тип, переполнение - еще раз и тд)
+//            System.out.println(output_str);
+//            System.out.println("Live:\t" + personLive + "\n");
+//
+//            System.out.println("Введите куда будет ходить персонаж(ход возможен только по вертикали и горизонтали на одну клетку;" +
+//                    "\nКоординаты персонажа - (x: " + personX + ", y: " + personY + "))");
+//            int x = sc.nextInt();
+//            int y = sc.nextInt();
+//            System.out.println(x + ", " + y);
+//
+//            // проверка
+//            if (x != personX && y != personY) {
+//                System.out.println("Неккоректный ход");
+//            } else if (Math.abs(x - personX) == 1) {
+//                personX = x;
+//                System.out.println("Ход корректный; Новые координаты: " + personX + ", " + personY);
+//            } else if (Math.abs(y - personY) == 1) {
+//                personY = y;
+//                System.out.println("Ход корректный; Новые координаты: " + personX + ", " + personY);
+//            }else {
+//                System.out.println("Координаты не изменены");
+//            }
+//
+//        } else if (answer.equals("НЕТ")) {
+//            System.out.println("Жаль, приходи еще!");
+//        } else {
+//            System.out.println("Данные введены неккоректно");
+//        }
+
+        switch (answer) {
+            case "ДА" -> {
+                System.out.println("Выбери сложность игры(от 1 до 5):");
+                int difficultGame = sc.nextInt();
+                System.out.println("Выбранная сложность:\t" + difficultGame);
+//                System.out.println("Сколько жизней будет у персонажа?");
+//            personLive = sc.nextInt(); // здесь необходимо рассказать о возможных ошибках(неверный тип, переполнение - еще раз и тд)
+
+                System.out.println(gamingField);
+                System.out.println("Количество жизней:\t" + personLive + "\n");
+                System.out.println("Введите куда будет ходить персонаж(ход возможен только по вертикали и горизонтали на одну клетку;" +
+                        "\nКоординаты персонажа - (x: " + personX + ", y: " + personY + "))");
+                int x = sc.nextInt();
+                int y = sc.nextInt();
+                System.out.println(x + ", " + y);
+
+                // проверка
+                if (x != personX && y != personY) {
+                    System.out.println("Неккоректный ход");
+                } else if (Math.abs(x - personX) == 1 || Math.abs(y - personY) == 1) {
+                    personX = x;
+                    personY = y;
+                    step++;
+                    System.out.println("Ход корректный; Новые координаты: " + personX + ", " + personY +
+                            "\nХод номер: " + step);
+                } else {
+                    System.out.println("Координаты не изменены");
+                }
+            }
+            case "НЕТ" -> System.out.println("Жаль, приходи еще!");
+            default -> System.out.println("Данные введены неккоректно");
+        }
 
     }
 }
